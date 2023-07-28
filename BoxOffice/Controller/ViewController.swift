9e13048efe858de4f833c50b510d4357c2d26254
Kobe <mskang@xingxingmobility.com>
2023-07-28 18:11:47 +0900

    와이의 캐리

--- BoxOffice/Controller/ViewController.swift
+++ BoxOffice/Controller/ViewController.swift
@@ -17,18 +17,30 @@ class ViewController: UIViewController {
         do {
             let networkManager = NetworkManager()
             let requestURL = try networkManager.makeURLRequest(
-                baseURL: "http://kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json?",
+                baseURL: "http://www.kobis.or.kr/kobisopenapi/webservice/rest/movie/searchMovieInfo.json?",
                 queryItems: [
                     URLQueryItem(name: "key", value: "d4bb1f8d42a3b440bb739e9d49729660"),
-                    URLQueryItem(name: "targetDt", value: "20230724"),
-                    URLQueryItem(name: "itemPerPage", value: "10"),
-                    URLQueryItem(name: "multiMovieYn", value: nil),
-                    URLQueryItem(name: "repNationCd", value: nil),
-                    URLQueryItem(name: "wideAreaCd", value: nil)
+                    URLQueryItem(name: "movieCd", value: "20124079")
                 ]
             )
-            networkManager.fetchData(requestURL: requestURL, sessionConfiguration: .default) { (boxOffice: BoxOffice) in
-                print(boxOffice)
+//            let requestURL = try networkManager.makeURLRequest(
+//                baseURL: "http://kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json?",
+//                queryItems: [
+//                    URLQueryItem(name: "key", value: "d4bb1f8d42a3b440bb739e9d49729660"),
+//                    URLQueryItem(name: "targetDt", value: "20230724"),
+//                    URLQueryItem(name: "itemPerPage", value: "10"),
+//                    URLQueryItem(name: "multiMovieYn", value: nil),
+//                    URLQueryItem(name: "repNationCd", value: nil),
+//                    URLQueryItem(name: "wideAreaCd", value: nil)
+//                ]
+//            )
+//            networkManager.fetchData(requestURL: requestURL, sessionConfiguration: .default) { (boxOffice: BoxOffice) in
+//                print(boxOffice)
+//            }
+            networkManager.fetchData(
+                requestURL: requestURL,
+                sessionConfiguration: .default) { (individualMovieDetailInfomation: IndividualMovieDetailInfomation) in
+                print("individualMovieDetailInfomation: \(individualMovieDetailInfomation)")
             }
         } catch {
             print(error.localizedDescription)
