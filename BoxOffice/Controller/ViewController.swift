4223a48b9313d2bcf05e9e8ee86cea2256d855cb
Kobe <mskang@xingxingmobility.com>
2023-08-09 10:44:50 +0900

    :recycle: Refactor: UI 작업 및 Item 작업.

--- BoxOffice/Controller/ViewController.swift
+++ BoxOffice/Controller/ViewController.swift
@@ -11,15 +11,20 @@ class ViewController: UIViewController, Fetchable {
     var boxOfficeData: [BoxOffice] = []
     var itemData: [Item] = []
     
-    
-//    var itemData: [DailyBoxOfficeList] = []
     let networkManager: NetworkManager = NetworkManager()
-    var dataSource: UICollectionViewDiffableDataSource<Section, DailyBoxOfficeList>? = nil
+    var dataSource: UICollectionViewDiffableDataSource<Section, Item>? = nil
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        printBoxOfficeData()
+        
+        
+        fetchBoxOfficeData { items in
+            DispatchQueue.main.async {
+                self.configureHierarchy()
+                self.configureDataSource()
+            }
+        }
     }
     
     func creatLayout() -> UICollectionViewLayout {
@@ -31,7 +36,17 @@ class ViewController: UIViewController, Fetchable {
     
     func configureHierarchy() {
         collectionView.autoresizingMask = [.flexibleHeight, .flexibleWidth]
+        
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        
         view.addSubview(collectionView)
+        
+        NSLayoutConstraint.activate([
+            collectionView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            collectionView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            collectionView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+            collectionView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+        ])
     }
     
     func configureDataSource() {
@@ -39,11 +54,11 @@ class ViewController: UIViewController, Fetchable {
             cell.updateWithItem(item)
             cell.accessories = [.disclosureIndicator()]
         }
-        dataSource = UICollectionViewDiffableDataSource<Section, DailyBoxOfficeList>(collectionView: collectionView, cellProvider: { (collectionView, indexPath, itemIdentifier) -> UICollectionViewCell in
+        dataSource = UICollectionViewDiffableDataSource<Section, Item>(collectionView: collectionView, cellProvider: { (collectionView, indexPath, itemIdentifier) -> UICollectionViewCell in
             return collectionView.dequeueConfiguredReusableCell(using: cellRegistration, for: indexPath, item: itemIdentifier)
         })
         
-        var snapshot = NSDiffableDataSourceSnapshot<Section, DailyBoxOfficeList>()
+        var snapshot = NSDiffableDataSourceSnapshot<Section, Item>()
         snapshot.appendSections([.main])
         snapshot.appendItems(itemData)
         guard let dataSource = dataSource else { return }
@@ -56,7 +71,7 @@ extension ViewController {
         do {
             
             let endPoint = EndPoint(
-                baseURL: "http://kobis.or.kr",
+                baseURL: "http://www.kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json",
                 queryItems: [
                     "key": "d4bb1f8d42a3b440bb739e9d49729660",
                     "targetDt": "20230724"
@@ -74,6 +89,47 @@ extension ViewController {
                 print(boxOffice)
             }
             
+        } catch {
+            print(error.localizedDescription)
+        }
+    }
+}
+
+extension ViewController {
+    func fetchBoxOfficeData(completion: @escaping ([Item]) -> ()) {
+        do {
+            let endPoint: EndPoint = EndPoint(
+                baseURL: "http://www.kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json",
+                queryItems: [
+                    "key": "d4bb1f8d42a3b440bb739e9d49729660",
+                    "targetDt": "20230724"
+                ]
+            )
+            
+            let url = try endPoint.generateURL(
+                isFullPath: false
+            )
+            
+            let urlRequest = URLRequest(url: url)
+            
+            networkManager.getBoxOfficeData(requestURL: urlRequest) { [weak self] (boxOffice: BoxOffice) in
+                guard let self = self else { return }
+                
+                self.boxOfficeData.append(boxOffice)
+                let count = boxOffice.boxOfficeResult.dailyBoxOfficeList.count
+                for index in 1...count {
+                    let rankNumber = boxOffice.boxOfficeResult.dailyBoxOfficeList[index-1].rankNumber
+                    let rankIntensity = boxOffice.boxOfficeResult.dailyBoxOfficeList[index-1].rankIntensity
+                    let movieName = boxOffice.boxOfficeResult.dailyBoxOfficeList[index-1].movieName
+                    let audienceCount = boxOffice.boxOfficeResult.dailyBoxOfficeList[index-1].audienceCount
+                    let audienceAccumulated = boxOffice.boxOfficeResult.dailyBoxOfficeList[index-1].audienceAccumulated
+                    
+                    let items = Item(rankNumber: rankNumber, rankIntensity: rankIntensity, movieName: movieName, audienceCount: audienceCount, audienceAccumulated: audienceAccumulated)
+                    
+                    self.itemData.append(items)
+                }
+                completion(self.itemData)
+            }
         } catch {
             print(error.localizedDescription)
         }
