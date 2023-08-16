895d826a7adac116b1058d5f1f467954d547fad8
Kobe <mskang@xingxingmobility.com>
2023-08-16 14:43:31 +0900

    :recycle: Refactor: DetailViewController 초기 필수 사항 구현.

--- BoxOffice/Controller/DetailViewController.swift
+++ BoxOffice/Controller/DetailViewController.swift
@@ -8,10 +8,21 @@
 import UIKit
 
 class DetailViewController: UIViewController {
-
+    var selectedMovieCode: String
+    
+    init(selectedMovieCode: String) {
+        self.selectedMovieCode = selectedMovieCode
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor = .systemPink
+        
     }
 
 }
