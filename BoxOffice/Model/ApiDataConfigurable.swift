4d6aa3a6f68952fabff46eb6f16405fad7c712aa
yyss99 <yyssjj9999@gmail.com>
2023-08-16 15:27:08 +0900

    ✨feat: ApiDataConfigurable, ApiDataNetworkConfig 생성 및 구현

---
+++ BoxOffice/Model/ApiDataConfigurable.swift
@@ -0,0 +1,26 @@
+//
+//  ApiDataConfigurable.swift
+//  BoxOffice
+//
+//  Created by by Kobe, yyss99 on 2023/08/16.
+//
+
+import Foundation
+
+public protocol ApiDataConfigurable {
+    var baseURL: URL { get }
+    var headers: [String: String] { get }
+    var queryParameters: [String: String] { get }
+}
+
+struct ApiDataNetworkConfig: ApiDataConfigurable {
+    public var baseURL: URL
+    public var headers: [String : String]
+    public var queryParameters: [String : String]
+    
+    public init(baseURL: URL, headers: [String: String], queryParameters: [String: String]) {
+        self.baseURL = baseURL
+        self.headers = headers
+        self.queryParameters = queryParameters
+    }
+}
